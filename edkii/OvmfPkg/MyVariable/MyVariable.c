889d4b9dc4926f96176b684ea9314b0a1f315ac8
xiaopangzi313 <1838786721@qq.com>
2020-11-21 13:51:54 +0800

    Create MyVariable.c
    add variable test demo

---
+++ edkii/OvmfPkg/MyVariable/MyVariable.c
@@ -0,0 +1,54 @@
+#include <uefi.h>
+#include <Library/UefiLib.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiRuntimeServicesTableLib.h>
+#include <Library/PcdLib.h>
+
+
+//ShellCEntryLib call user interface ShellAppMain
+EFI_STATUS
+EFIAPI
+MyHVariableAppEntry(
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+)
+{
+  EFI_STATUS                         Status = EFI_SUCCESS;
+  UINTN                              DataSize;
+  UINT64                             CSDNEnable = 1;
+  EFI_GUID  gEfiCsdnEnableGuid = { 0xb71604c2, 0xb8bd, 0x4dec, { 0x88, 0x78, 0xbc, 0xcf, 0x1d, 0xa2, 0x26, 0xc3 }};
+
+  DataSize = sizeof (CSDNEnable);
+  Status = gRT->SetVariable (
+                  L"CSDNEnable",
+                  &gEfiCsdnEnableGuid,
+                  EFI_VARIABLE_NON_VOLATILE |
+                  EFI_VARIABLE_BOOTSERVICE_ACCESS |
+                  EFI_VARIABLE_RUNTIME_ACCESS,
+                  DataSize,
+                  &CSDNEnable
+                  );
+
+  ASSERT(Status);
+  DEBUG ((EFI_D_ERROR," [CSDN]: Setvariable CSDNEnable 0x%x.\n", CSDNEnable));
+
+  CSDNEnable = 0;
+  DataSize = sizeof (CSDNEnable);
+  Status = gRT->GetVariable (
+                L"CSDNEnable",
+                &gEfiCsdnEnableGuid,
+                NULL,
+                &DataSize,
+                &CSDNEnable
+                );
+  ASSERT(Status);
+
+  if (CSDNEnable) {
+    DEBUG ((EFI_D_ERROR, "[CSDN] CSDNEnable\n"));
+  }
+
+  return EFI_SUCCESS;
+}
